d23a58e47cc8fa4b4dbefeed9f761f0d13bec72d
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-10-13 12:28:59 -0500

    ng-react-home-ok

--- Node-Express/React/src/components/home-component.js
+++ Node-Express/React/src/components/home-component.js
@@ -1,130 +1,119 @@
 import React, { Component } from "react";
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    componentDidMount() {      
-    }   
+  componentDidMount() {}
 
-    render() {
-
-        return (
-            <div className="container">
-                <div className="homePageHeader">
-                    <h3>[G5-G7] Math-Test / Grocery-MGT / Event-MGT</h3>
-                    <h5>Technology : Node JS / Express / JSON File Storage / React JS</h5>
+  render() {
+    return (
+      <div className="container">
+        <div className="homePageHeader">
+          <h3>[G5-G7] Math-Test / Grocery-MGT / Event-MGT</h3>
+          <h5>
+            Express / Node JS / bcrypt / JSON File Storage / React / Angular
+          </h5>
+        </div>
+        <hr />
+        <p></p>
+        <div className="row">
+          <div className="col-sm-5">
+            <div>
+              <ListGroup>
+                <ListGroup.Item variant="primary">
+                  <b>Login / Register</b>
+                </ListGroup.Item>
+                <div>
+                  <ul className="homeMathTest">
+                    <li>
+                      User can create Account by Registering with UserName,
+                      Password and Parent's Email Address
+                    </li>
+                    <li>
+                      User should Login with valid UserName and Password to
+                      access{" "}
+                      <u>
+                        <b>Math-Test and My-Events Module </b>
+                      </u>
+                    </li>
+                    <li>
+                      After successfully completion of any Math-Test, User's
+                      Parent will receive Test's Result by Email
+                    </li>
+                  </ul>
                 </div>
-                <hr />
-                <p></p>
-                <div className="row">
-                    <div className="col-sm-5">
-                        <div>
-                            <ListGroup>
-                                <ListGroup.Item variant="primary">
-                                    <b>Login / Register</b>
-                                </ListGroup.Item>
-                                <div>
-                                    <ul className="homeMathTest">
-                                        <li>
-                                            User can create Account by Registering with UserName, Password and Parent's Email Address
-                                        </li>
-                                        <li>
-                                            User should Login with valid UserName and Password to access <u><b>Math-Test and My-Events Module </b></u>
-                                        </li>
-                                        <li>
-                                            After successfully completion of any Math-Test, User's Parent will receive Test's Result by Email
-                                        </li>
-                                    </ul>                                 
-                                </div>
-                            </ListGroup>
-                        </div>
-                        <hr />
-                        <div>
-                            <ListGroup>
-                                <ListGroup.Item variant="primary">
-                                    <b>Grocery-MGT</b>
-                                </ListGroup.Item>
-                                <div>
-                                    <ul>
-                                        <li>Create Grocery List</li>
-                                        <li>Create New Item for Grocery List</li>
-                                        <li>Add Item To Grocery List By Check In</li>
-                                        <li>Remove Item From Grocery List By Check Out</li>
-                                    </ul>
-                                </div>
-                            </ListGroup>
-                        </div>
-                        <hr />
-                        <div>
-                            <ListGroup>
-                                <ListGroup.Item variant="primary">
-                                    <b>Event-MGT</b>
-                                </ListGroup.Item>
-                                <div>
-                                    <ul>
-                                        <li>User can Create/View/Edit and Delete Event</li>                                        
-                                        <li>User can View Events for Today, Current Week or Current Month</li>
-                                    </ul>
-                                </div>
-                            </ListGroup>
-                        </div>                                                                                 
-                    </div>
-                    <div className="col-sm-2">
-                    </div>
-                    <div className="col-sm-5">                        
-                        <div>
-                            <ListGroup>
-                                <ListGroup.Item variant="primary">
-                                    <b>Math-Test</b>
-                                </ListGroup.Item>
-                                <div>
-                                    <ul className="homeMathTest">
-                                        <li>
-                                            Every Question, Correct Answer and 2 Wrong Answers are generating dynamically
-                                        </li>
-                                        <li>
-                                            Every Time Questions are Different
-                                        </li>
-                                        <li>
-                                            Kids can experience real Math Calculation Problems
-                                        </li>
-                                    </ul>
-                                    <p></p>
-                                    <ul>
-                                        <li>
-                                            Addition [2 Digits by 2 Digits]
-                                        </li>
-                                        <li>
-                                            Addition [Decimal]
-                                        </li>
-                                        <li>
-                                            Minus [2 Digits by 2 Digits]
-                                        </li>
-                                        <li>
-                                            Minus [1 Digit by 2 Digits]
-                                        </li>
-                                        <li>
-                                            Minus [Decimal]
-                                        </li>
-                                        <li>
-                                            Multiply [2 Digits by 2 Digits]
-                                        </li>
-                                        <li>
-                                            Multiply [Decimal]
-                                        </li>
-                                        <li>
-                                            Division [2 Digits by 1 Digit]
-                                        </li>
-                                    </ul>
-                                </div>
-                            </ListGroup>
-                        </div>
-                    </div>
-                </div>
+              </ListGroup>
             </div>
-        );
-    }
+            <hr />
+            <div>
+              <ListGroup>
+                <ListGroup.Item variant="primary">
+                  <b>Grocery-MGT</b>
+                </ListGroup.Item>
+                <div>
+                  <ul>
+                    <li>Create Grocery List</li>
+                    <li>Create New Item for Grocery List</li>
+                    <li>Add Item To Grocery List By Check In</li>
+                    <li>Remove Item From Grocery List By Check Out</li>
+                  </ul>
+                </div>
+              </ListGroup>
+            </div>
+            <hr />
+            <div>
+              <ListGroup>
+                <ListGroup.Item variant="primary">
+                  <b>Event-MGT</b>
+                </ListGroup.Item>
+                <div>
+                  <ul>
+                    <li>User can Create/View/Edit and Delete Event</li>
+                    <li>
+                      User can View Events for Today, Current Week or Current
+                      Month
+                    </li>
+                  </ul>
+                </div>
+              </ListGroup>
+            </div>
+          </div>
+          <div className="col-sm-2"></div>
+          <div className="col-sm-5">
+            <div>
+              <ListGroup>
+                <ListGroup.Item variant="primary">
+                  <b>Math-Test</b>
+                </ListGroup.Item>
+                <div>
+                  <ul className="homeMathTest">
+                    <li>
+                      Every Question, Correct Answer and 2 Wrong Answers are
+                      generating dynamically
+                    </li>
+                    <li>Every Time Questions are Different</li>
+                    <li>Kids can experience real Math Calculation Problems</li>
+                  </ul>
+                  <p></p>
+                  <ul>
+                    <li>Addition [2 Digits by 2 Digits]</li>
+                    <li>Addition [Decimal]</li>
+                    <li>Minus [2 Digits by 2 Digits]</li>
+                    <li>Minus [1 Digit by 2 Digits]</li>
+                    <li>Minus [Decimal]</li>
+                    <li>Multiply [2 Digits by 2 Digits]</li>
+                    <li>Multiply [Decimal]</li>
+                    <li>Division [2 Digits by 1 Digit]</li>
+                  </ul>
+                </div>
+              </ListGroup>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- Node-Express/React/src/components/login-component.js
+++ Node-Express/React/src/components/login-component.js
@@ -2,231 +2,236 @@ import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 
 const formValid = ({ isError, ...rest }) => {
-    let isValid = false;
-    var BreakException = {};
-    try {
-        Object.values(isError).forEach(val => {
-            console.log('checking... ' + val);
-            if (val.length > 0) {
-                isValid = false
-                throw BreakException;
-            } else {
-                isValid = true
-            }
-        });
-    } catch (e) {
-        return isValid;
-    }
+  let isValid = false;
+  var BreakException = {};
+  try {
+    Object.values(isError).forEach((val) => {
+      console.log("checking... " + val);
+      if (val.length > 0) {
+        isValid = false;
+        throw BreakException;
+      } else {
+        isValid = true;
+      }
+    });
+  } catch (e) {
     return isValid;
+  }
+  return isValid;
 };
 
 export default class Login extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.resetLoginFormState = this.resetLoginFormState.bind(this);
+    this.resetLoginFormState = this.resetLoginFormState.bind(this);
 
-        this.state = {
-            parentEmail : '',
+    this.state = {
+      parentEmail: "",
 
-            apiResponse: '',
+      apiResponse: "",
 
-            userName: '',
-            password: '',
-            submitted: false,
-            isError: {
-                userName: '',
-                password: ''
-            }
-        }
+      userName: "",
+      password: "",
+      submitted: false,
+      isError: {
+        userName: "",
+        password: "",
+      },
+    };
+  }
+  componentDidMount() {
+    const user = this.getCurrentUser();
+    if (user) {
+      this.props.history.push("/home");
+      window.location.reload();
     }
-    componentDidMount() {
-        const user = this.getCurrentUser();
-        if (user) {
-            this.props.history.push('/home');
-            window.location.reload();
-        }
-    }
-    getCurrentUser() {
-        return JSON.parse(localStorage.getItem('userName'));
-    }
+  }
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("userName"));
+  }
 
-    onSubmit = e => {
-        e.preventDefault();
+  onSubmit = (e) => {
+    e.preventDefault();
 
-        if (formValid(this.state)) {
-            console.log(this.state)
-        } else {
-            console.log("Form is invalid!");
-            return;
-        }
-
-        // check for all null values @ form
-        if (this.state.userName == '' || this.state.password == '') {
-            if (this.state.userName == '') {
-                let isError = { ...this.state.isError };
-                isError.userName = "UserName : Required !";
-                this.setState({
-                    isError,
-                    submitted: false
-                });
-            }
-            if (this.state.password == '') {
-                let isError = { ...this.state.isError };
-                isError.password = "Password : Required !";
-                this.setState({
-                    isError,
-                    submitted: false
-                });
-            }
-            return;
-        }
-
-        // prepare data for api call
-        var data = {
-            userName: this.state.userName,
-            password: this.state.password
-        };      
-        // api call
-        fetch('/auth/login', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: { 'Content-Type': 'application/json' }
-            }).then(res => res.json())
-            .then(json => {
-                console.log(json);
-                if (json.error) {
-                    this.setState({
-                        apiResponse : json.error
-                    });
-                }
-                else {
-                    this.setState({
-                        // apiResponse: json.message
-                        apiResponse: 'Success!',
-                        parentEmail: json.parentEmail
-                    });
-                    localStorage.setItem("userName", JSON.stringify(data.userName));
-                    localStorage.setItem("parentEmail", JSON.stringify(json.parentEmail));
-                    setTimeout(() => {
-                        this.props.history.push('/home');
-                        window.location.reload();
-                    }, 2000);
-                    
-                }
-            }
-        );
+    if (formValid(this.state)) {
+      console.log(this.state);
+    } else {
+      console.log("Form is invalid!");
+      return;
     }
 
-    // reset login form state
-    resetLoginFormState() {
-        this.setState({
-            parentEmail: '',
-
-            apiResponse: '',
-            userName: '',
-            password: "",
-            submitted: false,
-            isError: {
-                userName: '',
-                password: ''
-            }
-        });
-    }
-
-    formValChange = e => {
-        e.preventDefault();
-        const { name, value } = e.target;
+    // check for all null values @ form
+    if (this.state.userName == "" || this.state.password == "") {
+      if (this.state.userName == "") {
         let isError = { ...this.state.isError };
-
-        switch (name) {
-            case "userName":
-                isError.userName =
-                    value.length < 4 ? "User Name : Atleast 4 characaters required" : "";
-                break;
-            case "password":
-                isError.password =
-                    value.length < 6 ? "Password : Atleast 6 characaters required" : "";
-                break;
-            default:
-                break;
-        }
+        isError.userName = "UserName : Required !";
         this.setState({
-            isError,
-            [name]: value
-        })
-    }
-   
-    doRegister = () => {
-        this.props.history.push('/register');
+          isError,
+          submitted: false,
+        });
+      }
+      if (this.state.password == "") {
+        let isError = { ...this.state.isError };
+        isError.password = "Password : Required !";
+        this.setState({
+          isError,
+          submitted: false,
+        });
+      }
+      return;
     }
 
-    render() {
-        const { isError, apiResponse } = this.state;      
+    // prepare data for api call
+    var data = {
+      userName: this.state.userName,
+      password: this.state.password,
+    };
+    // api call
+    fetch("/auth/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        if (json.error) {
+          this.setState({
+            apiResponse: json.error,
+          });
+        } else {
+          this.setState({
+            // apiResponse: json.message
+            apiResponse: "Success!",
+            parentEmail: json.parentEmail,
+          });
+          localStorage.setItem("userName", JSON.stringify(data.userName));
+          localStorage.setItem("parentEmail", JSON.stringify(json.parentEmail));
+          setTimeout(() => {
+            this.props.history.push("/home");
+            window.location.reload();
+          }, 2000);
+        }
+      });
+  };
 
-        return (
-            <form onSubmit={this.onSubmit} noValidate>
-                <Card style={{ width: '25rem' }}>
-                    <Card.Header>
-                        <h3 style={{ marginBottom: 30 }}>Login</h3>
-                        <p></p>
-                        <h5 className="loginErrorResponse">{apiResponse}</h5>
-                    </Card.Header>
-                    <Card.Body>
-                        <Card.Title>User Name</Card.Title>
-                        <Card.Text>
-                            <span className="form-group">
-                                <input
-                                    type="text"
-                                    className={isError.userName.length > 0 ? "is-invalid form-control" : "form-control"}
-                                    name="userName"
-                                    value={this.state.userName}
-                                    id="userName"
-                                    onChange={this.formValChange}
-                                />
-                                {isError.userName.length > 0 && (
-                                    <span className="invalid-feedback">{isError.userName}</span>
-                                )}
-                            </span>
-                        </Card.Text>
-                        <Card.Title>Password</Card.Title>
-                        <Card.Text>
-                            <span className="form-group">
-                                <input
-                                    type="password"
-                                    className={isError.password.length > 0 ? "is-invalid form-control" : "form-control"}
-                                    name="password"
-                                    value={this.state.password}
-                                    id="password"
-                                    onChange={this.formValChange}
-                                />
-                                {isError.password.length > 0 && (
-                                    <span className="invalid-feedback">{isError.password}</span>
-                                )}
-                            </span>
-                        </Card.Text>
-                        <p></p>
-                        <hr />
-                        <div className="row">
-                            <div className="col-sm-5">
-                                <button
-                                    type="submit"
-                                    className="btn btn-block btn-success">
-                                    <h5>Login</h5>
-                                </button>
-                            </div>
-                            <div className="col-sm-6">
-                                <button
-                                    type="button"
-                                    onClick={() => this.doRegister()}
-                                    className="btn btn-block btn-info">
-                                    <h5>Register</h5>
-                                </button>
-                            </div>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </form>
-        );
+  // reset login form state
+  resetLoginFormState() {
+    this.setState({
+      parentEmail: "",
+
+      apiResponse: "",
+      userName: "",
+      password: "",
+      submitted: false,
+      isError: {
+        userName: "",
+        password: "",
+      },
+    });
+  }
+
+  formValChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let isError = { ...this.state.isError };
+
+    switch (name) {
+      case "userName":
+        isError.userName =
+          value.length < 4 ? "User Name : Atleast 4 characaters required" : "";
+        break;
+      case "password":
+        isError.password =
+          value.length < 6 ? "Password : Atleast 6 characaters required" : "";
+        break;
+      default:
+        break;
     }
+    this.setState({
+      isError,
+      [name]: value,
+    });
+  };
+
+  doRegister = () => {
+    this.props.history.push("/register");
+  };
+
+  render() {
+    const { isError, apiResponse } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit} noValidate>
+        <Card style={{ width: "25rem" }}>
+          <Card.Header>
+            <h3 style={{ marginBottom: 30 }}>Login</h3>
+            <p></p>
+            <h5 className="loginErrorResponse">{apiResponse}</h5>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>User Name</Card.Title>
+            <Card.Text>
+              <span className="form-group">
+                <input
+                  type="text"
+                  className={
+                    isError.userName.length > 0
+                      ? "is-invalid form-control"
+                      : "form-control"
+                  }
+                  name="userName"
+                  value={this.state.userName}
+                  id="userName"
+                  onChange={this.formValChange}
+                />
+                {isError.userName.length > 0 && (
+                  <span className="invalid-feedback">{isError.userName}</span>
+                )}
+              </span>
+            </Card.Text>
+            <Card.Title>Password</Card.Title>
+            <Card.Text>
+              <span className="form-group">
+                <input
+                  type="password"
+                  className={
+                    isError.password.length > 0
+                      ? "is-invalid form-control"
+                      : "form-control"
+                  }
+                  name="password"
+                  value={this.state.password}
+                  id="password"
+                  onChange={this.formValChange}
+                />
+                {isError.password.length > 0 && (
+                  <span className="invalid-feedback">{isError.password}</span>
+                )}
+              </span>
+            </Card.Text>
+            <p></p>
+            <hr />
+            <div className="row">
+              <div className="col-sm-5">
+                <button type="submit" className="btn btn-block btn-success">
+                  <h5>Login</h5>
+                </button>
+              </div>
+              <div className="col-sm-6">
+                <button
+                  type="button"
+                  onClick={() => this.doRegister()}
+                  className="btn btn-block btn-info"
+                >
+                  <h5>Register</h5>
+                </button>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </form>
+    );
+  }
 }
