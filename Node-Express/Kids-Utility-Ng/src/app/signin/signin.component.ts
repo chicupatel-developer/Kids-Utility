ef6eba16eebaf538be2599b805ac8985ca331f6e
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-10-18 11:27:51 -0500

    ng-signin-ok

--- Node-Express/Kids-Utility-Ng/src/app/signin/signin.component.ts
+++ Node-Express/Kids-Utility-Ng/src/app/signin/signin.component.ts
@@ -10,8 +10,13 @@ import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+
+  baseServerUrl = 'http://localhost:5000/';
+  baseAuthUrl = 'auth/';
+
   responseColor = '';
-  errors = [];
+  apiResponse = '';
+  parentEmail = '';
 
   form: FormGroup = new FormGroup({
     UserName: new FormControl(''),
@@ -57,37 +62,46 @@ export class SigninComponent implements OnInit {
 
   onSubmit(): void {
     this.responseColor = '';
-    this.errors = [];  
+    this.apiResponse = '';
+    this.parentEmail = '';
 
     this.submitted = true;
 
     if (this.form.invalid) {
       return;
     }
-    
-      var userTokenData = {
-        UserName: '',
-        Token: '',
-        LoginTime: '',
-        ResponseCode: 0,
-        ResponseMessage: '',       
-        MyRole: ''
-      }
-          
-      this.signinModel.userName = this.form.value["UserName"];
-      this.signinModel.password = this.form.value["Password"];
+
+    this.signinModel.userName = this.form.value["UserName"];
+    this.signinModel.password = this.form.value["Password"];
       
     console.log(this.signinModel);
 
       // api call
-    fetch("http://localhost:5000/auth/login", {
+    fetch(this.baseServerUrl+this.baseAuthUrl+"login", {
       method: "POST",
       body: JSON.stringify(this.signinModel),
       headers: { "Content-Type": "application/json" },
     })
     .then((res) => res.json())
     .then((json) => {
-      console.log(json);      
+      console.log(json);   
+      if (json.error) {
+        this.responseColor = 'red';
+        this.apiResponse = json.error;
+        console.log('Error : '+this.apiResponse);
+      }
+      else {
+        this.responseColor = 'green';
+        this.apiResponse = "Success!";
+        this.parentEmail = json.parentEmail;
+        localStorage.setItem("userName", JSON.stringify(this.signinModel.userName));
+        localStorage.setItem("parentEmail", JSON.stringify(json.parentEmail));
+
+        setTimeout(() => {
+          this.onReset();
+          this.router.navigate(['/home']);
+        }, 3000);
+      }
     });
   }
 
@@ -95,6 +109,7 @@ export class SigninComponent implements OnInit {
     this.submitted = false;
     this.form.reset();
     this.responseColor = '';
-    this.errors = [];    
+    this.apiResponse = '';
+    this.parentEmail = '';
   }
 }
