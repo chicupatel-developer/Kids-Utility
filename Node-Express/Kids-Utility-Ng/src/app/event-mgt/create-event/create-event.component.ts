6b447b5cc4f8807bc7abd20c85aa0a79b649e5c2
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-10-23 14:42:36 -0500

    ng-event-create-wip3

--- Node-Express/Kids-Utility-Ng/src/app/event-mgt/create-event/create-event.component.ts
+++ Node-Express/Kids-Utility-Ng/src/app/event-mgt/create-event/create-event.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { UserService } from '../../services/user.service';
+import { LocalDataService } from '../../services/local-data.service';
 
 @Component({
   selector: 'app-create-event',
@@ -7,9 +12,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEventComponent implements OnInit {
 
-  constructor() { }
+  responseColor = '';
+  apiResponse = '';
+
+  form: FormGroup = new FormGroup({
+    eventDateValue: new FormControl(''),
+    eventTimeValue: new FormControl(''),
+    eventTitle: new FormControl(''),
+    eventDesc: new FormControl(''),
+  });
+  submitted = false;
+  eventModel = {
+    eventDate: '',
+    eventTitle: '',
+    eventDesc: '',
+    userName: '',
+    id: '',
+  };
+  
+  constructor(
+    public localDataService: LocalDataService,
+    public userService: UserService,
+    private formBuilder: FormBuilder,
+    public router: Router,
+  ) { }
 
   ngOnInit(): void {
+    if (this.userService.isLoggedIn) {
+      this.router.navigate(['/home']);
+    }
+   
+    this.form = this.formBuilder.group(
+      {
+        eventDateValue: [
+          '',
+          [
+            Validators.required,
+          ]
+        ],
+        eventTimeValue: [
+          '',
+          [
+            Validators.required,
+          ]
+        ],
+        eventTitle: [
+          '',
+          [
+            Validators.required,
+          ]
+        ],
+        eventDesc: [
+          '',
+          [
+            Validators.required,
+          ]
+        ],
+      
+      },
+    );
+  }
+
+    get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+    }
+  
+  onSubmit(): void {
+    this.responseColor = '';
+    this.apiResponse = '';
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);   
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+    this.responseColor = '';
+    this.apiResponse = '';
   }
 
 }
